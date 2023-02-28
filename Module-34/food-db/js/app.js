f1cbc3fd8370e7f1ca76da788f2e10175fa53ea6
Wixden <watchonlytutorial@gmail.com>
2023-02-28 21:13:11 +0600

    meal db created

---
+++ Module-34/food-db/js/app.js
@@ -0,0 +1,34 @@
+const loadUser = () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=f")
+    .then((res) => res.json())
+    .then((data) => displayUser(data));
+};
+
+const displayUser = (user) => {
+  const userContainer = document.getElementById("user-container");
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card", "shadow-xl", "my-10");
+  cardBody.innerHTML = `
+  <figure class="px-10 pt-10">
+    <img class="rounded-full"
+      src="${user.results[0].picture.large}"
+      alt="Album"
+    />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title mb-5 font-bold">User Details</h2>
+    <p><b>Name:</b> ${
+      user.results[0].name.first + " " + user.results[0].name.last
+    }</p>
+    <p><b>User Name:</b> ${user.results[0].login.username} </p>
+    <p><b>Phone:</b> ${user.results[0].phone}</p>
+    <p><b>Email:</b> ${user.results[0].email}</p>
+    <p><b>Address:</b> ${
+      user.results[0].location.city + ", " + user.results[0].location.country
+    }</p>
+  </div>
+  `;
+  userContainer.appendChild(cardBody);
+};
+
+loadUser();

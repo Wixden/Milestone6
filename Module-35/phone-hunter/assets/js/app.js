52f33b45f316445e0d044a9ee7306a28e63306ef
Wixden <watchonlytutorial@gmail.com>
2023-03-06 03:04:50 +0600

    Modal and data load complete

--- Module-35/phone-hunter/assets/js/app.js
+++ Module-35/phone-hunter/assets/js/app.js
@@ -1,14 +1,31 @@
 // Load Data
-const loadData = async (searchText) => {
+const loadData = async (searchText, limit) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   const res = await fetch(url);
   const data = await res.json();
-  displayAllData(data.data);
+  displayAllData(data.data, limit);
+
+  // const showAllButton = document.getElementById("show__all");
+  // showAllButton.addEventListener("click", () => {
+  //   // Start Spinner
+  //   if (data.data.length > 9) {
+  //     showAllButton.classList.add("d-none");
+  //     displayAllData(data.data);
+  //   } else {
+  //     showAllButton.classList.remove("d-none");
+  //   }
+  // });
 };
 
 // Display Data
-const displayAllData = (phones) => {
-  phones = phones.slice(0, 9);
+const displayAllData = (phones, limit) => {
+  const showAllButton = document.getElementById("show__all");
+  if (limit && phones.length > 10) {
+    phones = phones.slice(0, 9);
+    showAllButton.classList.remove("d-none");
+  } else {
+    showAllButton.classList.add("d-none");
+  }
 
   // No Data found
   const noPhone = document.getElementById("not__found");
@@ -49,7 +66,8 @@ const displayAllData = (phones) => {
             the bulk of the card's content.
           </p>
           <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-          <button type="button" class="btn btn-primary btn-md">
+          <button type="button" class="btn btn-primary btn-md" onclick="loadPhoneDetails('${phone.slug}')" data-bs-toggle="modal"
+          data-bs-target="#phoneDetailsModal">
             Read more
           </button>
         </div>
@@ -58,19 +76,81 @@ const displayAllData = (phones) => {
     `;
     phonesContainer.appendChild(phoneCard);
   }
+  // Stop Spinner
+  toggleSpinner(false);
+};
+
+// Limit search item to 9
+const searchLimit = (limit) => {
+  toggleSpinner(true);
+  const searchInput = document.getElementById("search__input");
+  const inputValue = searchInput.value;
+  loadData(inputValue, limit);
 };
 
 document.getElementById("searchBtn").addEventListener("click", () => {
-  const searchInput = document.getElementById("search__input");
-  const inputValue = searchInput.value;
-  loadData(inputValue);
+  searchLimit(9);
 });
 
-loadData();
+document
+  .getElementById("search__input")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      searchLimit(9);
+    }
+  });
+
+document.getElementById("show__all").addEventListener("click", () => {
+  searchLimit();
+});
+
+const toggleSpinner = (isLoading) => {
+  const spinnerContainer = document.getElementById("spinner");
+  if (isLoading) {
+    spinnerContainer.classList.remove("d-none");
+  } else {
+    spinnerContainer.classList.add("d-none");
+  }
+};
+
+const loadPhoneDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhoneDetails(data.data);
+};
+
+const displayPhoneDetails = (phone) => {
+  const title = document.getElementById("phoneDetailsModalLabel");
+  title.innerText = phone.name;
+  const modalBody = document.getElementById("modal__body");
+  modalBody.innerHTML = `
+  <h4 class="mb-4">All Features</h4>
+  <p><b>Brand:<b/> ${phone.brand}</p>
+  <p><b>Chipset:</b> ${phone.mainFeatures.chipSet}</p>
+  <p><b>Display:</b> ${phone.mainFeatures.displaySize}</p>
+  <p><b>Memory:</b> ${phone.mainFeatures.memory}</p>
+  <p><b>Storage:</b> ${phone.mainFeatures.storage}</p>
+  `;
+};
 
 const scrollButton = document.getElementById("searchBtn");
+const inputSearch = document.getElementById("search__input");
 const mainSection = document.querySelector("main");
 
 scrollButton.addEventListener("click", () => {
-  mainSection.scrollIntoView({ behavior: "smooth" });
+  setTimeout(() => {
+    mainSection.scrollIntoView({ behavior: "smooth" });
+  }, 100); // wait for 100 milliseconds before scrolling
 });
+
+// Show result on Enter Key press
+inputSearch.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      mainSection.scrollIntoView({ behavior: "smooth" });
+    }, 100); // wait for 100 milliseconds before scrolling
+  }
+});
+
+loadData("apple");
